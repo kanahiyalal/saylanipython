684d96c792c28625bc84b588a55c976856483696
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-22 15:52:34 +0000

    course

---
+++ first.py
@@ -0,0 +1,13 @@
+
+name = input("what is your name:")
+print("hello:"+name)
+age = input("what is your age:")
+print("age:"+age)
+address = input("what is your address:")
+print("address:"+address)
+contact = input("what is your contact:")
+print("contact:"+contact)
+height = input("what is your height:")
+print("height:"+height)
+ 
+ 
---
+++ second.py
@@ -0,0 +1,3 @@
+name = input("what is your name:")
+age = input("what is your age:")
+print("hello "+ name + "  !you are "+ age + " years old." )
